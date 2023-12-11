b911e4fbc79acdbec7144fd0569bdb68938d3b19
Nischay2312 <nischayjoshi2312@gmail.com>
2023-12-11 14:58:55 -0800

    Added matlab to process and vsualize gps data

---
+++ matlab/GPS_Process.m
@@ -0,0 +1,56 @@
+close all
+clear all
+% Define the file path
+file_path = '../RecordedData/GPS_DATA_Parsed.txt'; % Replace with your file path
+
+% Read the data into a table
+GPS_data = ReadParsedGPS(file_path);
+Data_Length = height(GPS_data);
+
+% Reference and new coordinates
+R_lat = GPS_data.latitude(1); % Reference latitude
+R_long = GPS_data.longitude(1); % Reference longitude
+R_alt = GPS_data.altitude_m_(1); % Reference Altitude
+
+% Make arrays to store the X and Y position moved
+X_pos = zeros(1, Data_Length)';
+Y_pos = zeros(1, Data_Length)';
+Z_pos = zeros(1, Data_Length)';
+
+% Iterate through all the data points
+for i = 1:Data_Length
+    % Current point's coordinates and altitude
+    C_lat = GPS_data.latitude(i);
+    C_long = GPS_data.longitude(i);
+    C_alt = GPS_data.altitude_m_(i);
+
+    % Calculate X and Y distances (flat Earth approximation)
+    X_pos(i) = distanceBetween(R_lat, R_long, R_lat, C_long);
+    Y_pos(i) = distanceBetween(R_lat, R_long, C_lat, R_long);
+    
+    % Check direction (East/West for X, North/South for Y)
+    if C_long < R_long
+        X_pos(i) = -X_pos(i);
+    end
+    if C_lat < R_lat
+        Y_pos(i) = -Y_pos(i);
+    end
+
+    % Calculate Z distance (altitude difference)
+    Z_pos(i) = C_alt - R_alt;
+end
+
+% 3D plot of the path
+figure;
+plot3(X_pos, Y_pos, Z_pos, 'o-');
+xlabel('X Position (m)');
+ylabel('Y Position (m)');
+zlabel('Z Position (m)');
+title('3D Path of GPS Data');
+grid on;
+% Set axis limits
+xlim([min(X_pos)-10, max(X_pos)+10]); % Extend limits by 10 meters on each side for X
+ylim([min(Y_pos)-10, max(Y_pos)+10]); % Extend limits by 10 meters on each side for Y
+zlim([-100, 100]); % Extend limits by 10 meters on each side for Z
+figure;
+plot(X_pos,Y_pos);
---
+++ matlab/ReadParsedGPS.m
@@ -0,0 +1,8 @@
+function GPSDATA = ReadParsedGPS(file_location)
+    % format long g
+    % Read the data into a table
+    opts = detectImportOptions(file_location, 'Delimiter', '\t');
+    opts = setvartype(opts, {'latitude', 'longitude', 'altitude_m_', 'course', 'speed_km_h_', 'hdop' }, 'double');
+    GPSDATA = readtable(file_location, opts);
+end
+    
