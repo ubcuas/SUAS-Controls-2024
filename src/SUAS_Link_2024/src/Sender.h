4270ea6b3a1f5112254410c1b89c48e18d8b8ff0
p-eanat <amyinventions@gmail.com>
2024-03-30 16:55:26 -0700

    Fix espNow code

--- src/SUAS_Link_2024/src/Sender.h
+++ src/SUAS_Link_2024/src/Sender.h
@@ -2,24 +2,15 @@
 #define SENDER_H
 
 #include <Arduino.h>
-// // #include <esp_now.h>
+#include <esp_now.h>
 // #include <ESPNowW.h>
-// #include <WiFi.h>
+#include <WiFi.h>
 
-// #include "params.h"
+#include "params.h"
 
-// #define DEBUG 1
-// #define TIMEOUT_MILLIS 5000
-// #define TIMEOUT_ERROR 2
-
-// // Reading From Serial
-// bool initialized = false;
-// int incomingByte = 0;
-// char *serialVals[3]; 
-// char *ptr = NULL;
-
-// // MAC Address of responder - edit as required
-// uint8_t broadcastAddressAll[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+#define DEBUG 1
+#define TIMEOUT_MILLIS 5000
+#define TIMEOUT_ERROR 2
  
 // Define a sending structure
 typedef struct struct_message {
@@ -29,19 +20,15 @@ typedef struct struct_message {
   int bottleID;
 } struct_message;
  
-// // Define response structure
-// typedef struct struct_response {
-//   int bottleID;
-// } struct_response;
-// bool success = false;
+// Define response structure
+typedef struct struct_response {
+  int bottleID;
+} struct_response;
 
-// esp_now_peer_info_t peerInfo;
-// struct_response response;
-
-// void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
-// void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len);
-// bool broadcastMessage(struct_message myData);
-// struct_message recieveData();
-// bool handShakeSerial();
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len);
+bool broadcastMessage(struct_message myData);
+struct_message recieveData();
+bool handShakeSerial();
 
 #endif
